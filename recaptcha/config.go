437cc9de659b5f8b873e2edad75186d0413c1711
Chen Zhen <zhen.chen@smartswarm.org>
2020-03-24 23:30:07 +0800

    remove site-secret in config

--- recaptcha/config.go
+++ recaptcha/config.go
@@ -3,28 +3,25 @@ package recaptcha
 import "errors"
 
 var (
-	in_sec      string
-	in_site_sec string
-	in_host     string
+	in_sec  string
+	in_host string
 )
 
 // Config init config info
-func Config(sec, site_sec string) {
-	ConfigWithHost(sec, site_sec, "www.google.com")
+func Config(sec string) {
+	ConfigWithHost(sec, "www.google.com")
 }
 
 // ConfigWithHost init config info
 // host should be one of:
 // 	www.google.com
 // 	www.recaptcha.net
-func ConfigWithHost(sec, site_sec string, host string) {
+func ConfigWithHost(sec string, host string) {
 	if sec == "" {
 		panic(errors.New("require secret of reCAPTCHA"))
-	} else if site_sec == "" {
-		panic(errors.New("require site secret of reCAPTCHA"))
 	} else if host == "" {
 		panic(errors.New("require host of reCAPTCHA"))
 	}
 
-	in_sec, in_site_sec, in_host = sec, site_sec, host
+	in_sec, in_host = sec, host
 }
