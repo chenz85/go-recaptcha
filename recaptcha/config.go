bb30bdb5f7dec32bf65c134ecc9dc959fee5ef38
Chen Zhen <zhen.chen@smartswarm.org>
2020-03-24 22:52:25 +0800

    config with host

--- recaptcha/config.go
+++ recaptcha/config.go
@@ -5,15 +5,24 @@ import "errors"
 var (
 	in_sec      string
 	in_site_sec string
+	in_host     string
 )
 
 // Config init config info
 func Config(sec, site_sec string) {
+	ConfigWithHost(sec, site_sec, "www.google.com")
+}
+
+// ConfigWithHost init config info
+// host should be one of:
+// 	www.google.com
+// 	www.recaptcha.net
+func ConfigWithHost(sec, site_sec string, host string) {
 	if sec == "" {
 		panic(errors.New("require secret of reCAPTCHA"))
 	} else if site_sec == "" {
 		panic(errors.New("require site secret of reCAPTCHA"))
 	}
 
-	in_sec, in_site_sec = sec, site_sec
+	in_sec, in_site_sec, in_host = sec, site_sec, host
 }
